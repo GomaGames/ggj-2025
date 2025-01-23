86d11c091ee0370f67065e533d4fd7aae7c6f76c
theRemix <jon@gomagames.com>
2025-01-22 23:50:21 -0800

    feat(fire): projectile bubbles
    fired in direction that player is facing
    
    SmallBubble extends Bubble
    
    resolves #7

--- Sprites/player.gd
+++ Sprites/player.gd
@@ -4,7 +4,8 @@ extends CharacterBody2D
 @export var WALK_FORCE:float = 600.0
 @export var WALK_MAX_SPEED:float = 200
 @export var STOP_FORCE:float = 1300
-@export var JUMP_SPEED = 200
+@export var JUMP_SPEED:float = 200
+@export var FIRE_FORCE:float = 500
 
 @export var player_num:int = 0 # must be 1-4
 @export var physics_enabled:bool = true
@@ -12,6 +13,12 @@ extends CharacterBody2D
 @export var color:Color:
 	set(value):
 		(get_node("Polygon2D") as Polygon2D).color = value
+
+@export var bubbles_container:Node
+@onready var small_bubble_scene = preload("res://Sprites/SmallBubble.tscn")
+
+@onready var facing:Node2D = $"Facing"
+@onready var fireOriginPoint:Marker2D = $"Facing/FireOriginPoint"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,9 +31,16 @@ func _process(delta: float) -> void:
 func _physics_process(delta):
 	if !physics_enabled:
 		return
-		
+	
+	handle_movement(delta)
+	handle_fire()
+	handle_bash()
+	
+func handle_movement(delta):
+	# keyboard input
 	var walk := WALK_FORCE * (Input.get_axis(&"p%s_left" % player_num, &"p%s_right" % player_num))
 	
+	# gamepad input
 	if walk == 0:
 		walk = WALK_FORCE * Input.get_joy_axis(player_num-1,JOY_AXIS_LEFT_X)
 	
@@ -39,6 +53,12 @@ func _physics_process(delta):
 	
 	# Clamp to the maximum horizontal movement speed.
 	velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
+	
+	# flip facing direction
+	if velocity.x < 0:
+		facing.scale = Vector2(-1, 1)
+	elif velocity.x > 0:
+		facing.scale = Vector2(1, 1)
 
 	# apply gravity
 	velocity.y += delta * GRAVITY
@@ -48,3 +68,17 @@ func _physics_process(delta):
 	# Check for jumping. is_on_floor() must be called after movement code.
 	if is_on_floor() and Input.is_action_just_pressed(&"p%s_jump" % player_num):
 		velocity.y = -JUMP_SPEED
+
+func handle_fire():
+	if bubbles_container == null:
+		return
+		
+	if Input.is_action_just_released(&"p%s_fire" % player_num):
+		var sm_bubble:RigidBody2D = small_bubble_scene.instantiate()
+		bubbles_container.add_child(sm_bubble)
+		sm_bubble.global_position = fireOriginPoint.global_position
+		sm_bubble.linear_velocity = Vector2(facing.scale.x * FIRE_FORCE, 0)
+		
+		
+func handle_bash():
+	pass
